a44b317f38d425d3e14db4acaa52fae5cb3c9aa3
Mananchery Panneerselvam <I29140@verisk.com>
2020-04-19 02:05:17 -0600

    Updates

---
+++ precache-manifest.f7add047d8ec78e50d6452252cfb1423.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3907242e5e7e03d5894492c5dcf112",
+    "url": "/covid19_corona_tracker/index.html"
+  },
+  {
+    "revision": "aac8ea50ab4741106425",
+    "url": "/covid19_corona_tracker/static/css/main.cbd39092.chunk.css"
+  },
+  {
+    "revision": "07cd9e48cd07889c0b8f",
+    "url": "/covid19_corona_tracker/static/js/2.ddd474aa.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19_corona_tracker/static/js/2.ddd474aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aac8ea50ab4741106425",
+    "url": "/covid19_corona_tracker/static/js/main.08818676.chunk.js"
+  },
+  {
+    "revision": "672d4bbb8f6b8f7b1cc3",
+    "url": "/covid19_corona_tracker/static/js/runtime-main.b83eaaba.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19_corona_tracker/static/media/image.d7265326.png"
+  }
+]);
